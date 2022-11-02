127f3321a2bba7133a89fe993ed6bed3c4ceb0ab
Georgycas <32694172+Georgycas@users.noreply.github.com>
2022-11-02 11:10:05 -0700

    finished 2.4

--- CE450HW/Week 2/2.4.py
+++ CE450HW/Week 2/2.4.py
@@ -1,18 +1,30 @@
-# Ask the professor this weird input
-def add(a, b):
-    return a + b
+def incr(a):
+    return a + 1
 
 
-def foo(f, n):
+def square(a):
+    return a ** 2
+
+
+def triple(a):
+    return a * 3
+
+
+def foo(f, n=1):
     def g(m):
-        return f(n, m)
+        count = 1
+        x = f(m)
+        while count < n:
+            count = count + 1
+            x = f(x)
+        return x
 
     return g
 
 
-foo(add, 2)(1)
-"""add3 = foo(incr, 5)
-add3(3)
-foo(triple, 5)(1)
-foo(square, 2)(5)
-foo(square, 4)(5)"""
+print(foo(incr, 9)(8))
+add3 = foo(incr, 5)
+print(add3(3))
+print(foo(triple, 5)(1))
+print(foo(square, 2)(5))
+print(foo(square, 4)(5))
