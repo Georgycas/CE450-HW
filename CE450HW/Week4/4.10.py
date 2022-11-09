fad690e64b2b81a3ea73cf4a99a52251d1bc3879
Georgycas <32694172+Georgycas@users.noreply.github.com>
2022-11-09 15:46:40 -0800

    Update 4.10.py

--- CE450HW/Week4/4.10.py
+++ CE450HW/Week4/4.10.py
@@ -1,9 +1,24 @@
 def crte_2d_arr(rows, columns):
-    x=[]
+    x = []
+    n = []
 
-    
-    """
-    >>> crte_2d_arr(3, 5)
-    [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-']]
+    def col(z, i=0):
+        if i == z:
+            return
+        else:
+            x.append('_')
+        col(z, i + 1)
 
-    """
+    def row(z, i=0):
+        if z == i:
+            return
+        else:
+            n.append(x)
+        row(z, i + 1)
+
+    col(columns)
+    row(rows)
+    return n
+
+
+print(crte_2d_arr(3, 5))
